d69749711fb6c341500c77223c8f3158547cdd79
Saikiran Ambati <ambati1995@gmail.com>
2018-01-03 19:19:15 +0530

    Getting_Started

---
+++ US Bike Share Data/Bike_share_analysis.py
@@ -0,0 +1,54 @@
+# coding: utf-8
+
+# # 2016 US Bike Share Activity Snapshot
+# 
+# ## Table of Contents
+# - [Introduction](#intro)
+# - [Posing Questions](#pose_questions)
+# - [Data Collection and Wrangling](#wrangling)
+#   - [Condensing the Trip Data](#condensing)
+# - [Exploratory Data Analysis](#eda)
+#   - [Statistics](#statistics)
+#   - [Visualizations](#visualizations)
+# - [Performing Your Own Analysis](#eda_continued)
+# - [Conclusions](#conclusions)
+# 
+# <a id='intro'></a>
+# ## Introduction
+# 
+# > **Tip**: Quoted sections like this will provide helpful instructions on how to navigate and use a Jupyter notebook.
+# 
+# Over the past decade, bicycle-sharing systems have been growing in number and popularity in cities across the world. Bicycle-sharing systems allow users to rent bicycles for short trips, typically 30 minutes or less. Thanks to the rise in information technologies, it is easy for a user of the system to access a dock within the system to unlock or return bicycles. These technologies also provide a wealth of data that can be used to explore how these bike-sharing systems are used.
+# 
+# In this project, you will perform an exploratory analysis on data provided by [Motivate](https://www.motivateco.com/), a bike-share system provider for many major cities in the United States. You will compare the system usage between three large cities: New York City, Chicago, and Washington, DC. You will also see if there are any differences within each system for those users that are registered, regular users and those users that are short-term, casual users.
+
+# <a id='pose_questions'></a>
+# ## Posing Questions
+# 
+# Before looking at the bike sharing data, you should start by asking questions you might want to understand about the bike share data. Consider, for example, if you were working for Motivate. What kinds of information would you want to know about in order to make smarter business decisions? If you were a user of the bike-share service, what factors might influence how you would want to use the service?
+# 
+# **Question 1**: Write at least two questions related to bike sharing that you think could be answered by data.
+# 
+# **Answer**: 
+#  >> 1) Average time spent by a person on bike <br>
+#  >> 2) peak hours where there is an increased demand for Bikes to include Surge pricing. <br>
+#  >> 3) Repeated Customers? <br>
+#  >> 4) effect of demographic conditions or effect of days (weekday / weekend)? <br>
+# 
+# 
+# > **Tip**: If you double click on this cell, you will see the text change so that all of the formatting is removed. This allows you to edit this block of text. This block of text is written using [Markdown](http://daringfireball.net/projects/markdown/syntax), which is a way to format text using headers, links, italics, and many other options using a plain-text syntax. You will also use Markdown later in the Nanodegree program. Use **Shift** + **Enter** or **Shift** + **Return** to run the cell and show its rendered form.
+
+# <a id='wrangling'></a>
+# ## Data Collection and Wrangling
+# 
+# Now it's time to collect and explore our data. In this project, we will focus on the record of individual trips taken in 2016 from our selected cities: New York City, Chicago, and Washington, DC. Each of these cities has a page where we can freely download the trip data.:
+# 
+# - New York City (Citi Bike): [Link](https://www.citibikenyc.com/system-data)
+# - Chicago (Divvy): [Link](https://www.divvybikes.com/system-data)
+# - Washington, DC (Capital Bikeshare): [Link](https://www.capitalbikeshare.com/system-data)
+# 
+# If you visit these pages, you will notice that each city has a different way of delivering its data. Chicago updates with new data twice a year, Washington DC is quarterly, and New York City is monthly. **However, you do not need to download the data yourself.** The data has already been collected for you in the `/data/` folder of the project files. While the original data for 2016 is spread among multiple files for each city, the files in the `/data/` folder collect all of the trip data for the year into one file per city. Some data wrangling of inconsistencies in timestamp format within each city has already been performed for you. In addition, a random 2% sample of the original data is taken to make the exploration more manageable. 
+# 
+# **Question 2**: However, there is still a lot of data for us to investigate, so it's a good idea to start off by looking at one entry from each of the cities we're going to analyze. Run the first code cell below to load some packages and functions that you'll be using in your analysis. Then, complete the second code cell to print out the first trip recorded from each of the cities (the second line of each data file).
+# 
+# > **Tip**: You can run a code cell like you formatted Markdown cells above by clicking on the cell and using the keyboard shortcut **Shift** + **Enter** or **Shift** + **Return**. Alternatively, a code cell can be executed using the **Play** button in the toolbar after selecting it. While the cell is running, you will see an asterisk in the message to the left of the cell, i.e. `In [*]:`. The asterisk will change into a number to show that execution has completed, e.g. `In [1]`. If there is output, it will show up as `Out [1]:`, with an appropriate number to match the "In" number.
