515cff4b9bfc54226dbb5eed12ecb1dff3fda8f5
Saikiran Ambati <ambati1995@gmail.com>
2018-01-29 14:09:07 -0600

    initiate

---
+++ Exploring The movie Data base/part 1.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+# In[1]:
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+get_ipython().magic('matplotlib inline')
+
+df = pd.read_csv('tmdb-movies.csv')
+
