68af2b7160f6128bb3a03d03b51eae130ae8b9f8
Saikiran Ambati <ambati1995@gmail.com>
2018-01-29 14:09:42 -0600

    describe_mean

--- Exploring The movie Data base/part 1.py
+++ Exploring The movie Data base/part 1.py
@@ -9,3 +9,18 @@ get_ipython().magic('matplotlib inline')
 
 df = pd.read_csv('tmdb-movies.csv')
 
+# In[2]:
+
+df.head(2)
+
+
+# In[3]:
+
+df.info()
+
+
+# ## mean_revenue, mean_budget over the year
+
+# In[4]:
+
+yearly_stats = df.groupby('release_year').mean().reset_index()
