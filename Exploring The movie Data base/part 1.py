2e5b6b28c5bb2179f28d8126022dc8d67c269672
Saikiran Ambati <ambati1995@gmail.com>
2018-01-30 19:27:05 -0600

    Revenue_means

--- Exploring The movie Data base/part 1.py
+++ Exploring The movie Data base/part 1.py
@@ -25,3 +25,18 @@ df.info()
 
 yearly_stats = df.groupby('release_year').mean().reset_index()
 
+# In[5]:
+
+
+budget_means = yearly_stats['budget']
+
+
+ind = np.arange(1960,2016)  # the x locations for the groups
+width = 0.35       # the width of the bars
+
+fig, ax = plt.subplots()
+rects1 = ax.bar(ind, budget_means, width, color='r')
+
+revenue_means = yearly_stats['revenue']
+
+
