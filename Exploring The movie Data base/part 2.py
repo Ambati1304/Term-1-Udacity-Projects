c529bac8ec51ce36c4fabc4cda654f617e0da347
Saikiran Ambati <ambati1995@gmail.com>
2018-02-03 10:42:19 -0600

    loading_data

--- Exploring The movie Data base/part 2.py
+++ Exploring The movie Data base/part 2.py
@@ -1,3 +1,15 @@
 # coding: utf-8
 
 # # Which genres are most popular from year to year?
+
+# 
+# >import libraries and read the data set
+
+# In[28]:
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+get_ipython().magic('matplotlib inline')
+
+df = pd.read_csv('tmdb-movies.csv')
