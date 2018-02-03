20b478b7c5211782e5f4e9f53ee53962381d9fa8
Saikiran Ambati <ambati1995@gmail.com>
2018-02-03 10:44:20 -0600

    cleaning_data

--- Exploring The movie Data base/part 2.py
+++ Exploring The movie Data base/part 2.py
@@ -13,3 +13,42 @@ import matplotlib.pyplot as plt
 get_ipython().magic('matplotlib inline')
 
 df = pd.read_csv('tmdb-movies.csv')
+
+# > shape of the data frame
+
+# In[5]:
+
+df.shape
+
+
+# > Columns of the data frame for better understanding
+
+# In[6]:
+
+df.columns
+
+
+# > visuvalizing the head of data
+
+# In[7]:
+
+df.head(2)
+
+
+# >Description of data to clear the null values
+
+# In[8]:
+
+df.info()
+
+
+# >The column ‘popularity’ has no NaN, so it not needed to drop nulls, but the value counts of the popularity has long list of float values. Which will be hard to analyze. So, we round of the popularity column to nearest integer.
+
+# In[9]:
+
+df['popularity']= round(df['popularity'])
+
+
+# In[10]:
+
+df.head(2)
